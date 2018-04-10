11056cc01abbae990ae1f3c17883dd691e9aed78
light <halysl0817@gmail.com>
2018-04-10 14:27:27 +0800

    move to v1.0

--- dict.py
+++
@@ -1,9 +0,0 @@
-import string
-li = []
-with open('name.txt','r') as f:
-    for line in f.readlines():
-        a = line[:-2]
-        li.append(a)
-        print(a)
-
-print(li)
--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-import requests
-
-for i in range(1,123):
-    os.system('mkdir /Users/light/PycharmProjects/spider/lol/pic/skin/' + str(i))
