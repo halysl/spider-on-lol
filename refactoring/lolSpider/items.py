9b09b8084d627616d3615172e900eb1371fe875c
yagami light <halysl0817@gmail.com>
2018-09-02 18:30:57 +0800

    增加物品信息和物品图片的采集

--- refactoring/lolSpider/items.py
+++ refactoring/lolSpider/items.py
@@ -8,10 +8,9 @@
 import scrapy
 
 
-# 对应LOL-Hero-Name
 class LOLHeroInfoSpiderItem(scrapy.Item):
     """
-    继承自scrapy.Item，在spiders/lol-hero-name-spidder.py中使用
+    继承自scrapy.Item，在spiders/lol-hero-info-spider.py中使用
     """
     # name英雄名称，e_name英雄英文名称（即英雄id），title英雄称号
     hero_detail_url = scrapy.Field()
@@ -25,11 +24,16 @@ class LOLHeroInfoSpiderItem(scrapy.Item):
 
 # 对应LOL-Item-Name
 class LOLItemInfoSpiderItem(scrapy.Item):
-    # item_id物品id，item_name物品名称
     item_id = scrapy.Field()
-    item_name = scrapy.Field()    
+    item_name = scrapy.Field()
+    # 物品功能描述
+    item_func = scrapy.Field()
+    # 物品简介
+    item_desc = scrapy.Field()
+    # price is a dict,include base purchasable total sell
     item_price = scrapy.Field()
-    item_image_urls = scrapy.Field()
+    item_tag = scrapy.Field()
+    item_image_url = scrapy.Field()
 
 
 class LOLHeroSkinSpiderItem(scrapy.Item):
